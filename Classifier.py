5da3df2363db6e076efff9e059b724a991482e9f
unknown <reynald.gresik@gmail.com>
2021-05-02 22:01:01 +0700

    add all modul

---
+++ Classifier.py
@@ -0,0 +1,54 @@
+from keras import backend as K
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Flatten, Dense, Input, Conv2D, MaxPooling2D, Dropout
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, TimeDistributed
+from keras.engine.topology import get_source_inputs
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.objectives import categorical_crossentropy
+
+from keras.models import Model
+from keras.utils import generic_utils
+from keras.engine import Layer, InputSpec
+from keras import initializers, regularizers
+
+# """Create a classifier layer
+
+# Args:
+#     base_layers: vgg
+#     input_rois: `(1,num_rois,4)` list of rois, with ordering (x,y,w,h)
+#     num_rois: number of rois to be processed in one time (4 in here)
+
+# Returns:
+#     list(out_class, out_regr)
+#     out_class: classifier layer output
+#     out_regr: regression layer output
+# """
+
+from RoiPolling import RoiPoolingConv
+
+
+def classifier_layer(base_layers, input_rois, num_rois, nb_classes = 4):
+    input_shape = (num_rois,7,7,512)
+
+    pooling_regions = 7
+
+    # out_roi_pool.shape = (1, num_rois, channels, pool_size, pool_size)
+    # num_rois (4) 7x7 roi pooling
+    out_roi_pool = RoiPoolingConv(pooling_regions, num_rois)([base_layers, input_rois])
+
+    # Flatten the convlutional layer and connected to 2 FC and 2 dropout
+    out = TimeDistributed(Flatten(name='flatten'))(out_roi_pool)
+    out = TimeDistributed(Dense(4096, activation='relu', name='fc1'))(out)
+    out = TimeDistributed(Dropout(0.5))(out)
+    out = TimeDistributed(Dense(4096, activation='relu', name='fc2'))(out)
+    out = TimeDistributed(Dropout(0.5))(out)
+
+    # There are two output layer
+    # out_class: softmax acivation function for classify the class name of the object
+    # out_regr: linear activation function for bboxes coordinates regression
+    out_class = TimeDistributed(Dense(nb_classes, activation='softmax', kernel_initializer='zero'), name='dense_class_{}'.format(nb_classes))(out)
+    # note: no regression target for bg class
+    out_regr = TimeDistributed(Dense(4 * (nb_classes-1), activation='linear', kernel_initializer='zero'), name='dense_regress_{}'.format(nb_classes))(out)
+
+    return [out_class, out_regr]
---
+++ RPN.py
@@ -0,0 +1,37 @@
+from keras import backend as K
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Flatten, Dense, Input, Conv2D, MaxPooling2D, Dropout
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, TimeDistributed
+from keras.engine.topology import get_source_inputs
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.objectives import categorical_crossentropy
+
+from keras.models import Model
+from keras.utils import generic_utils
+from keras.engine import Layer, InputSpec
+from keras import initializers, regularizers
+
+# """Create a rpn layer
+#     Step1: Pass through the feature map from base layer to a 3x3 512 channels convolutional layer
+#             Keep the padding 'same' to preserve the feature map's size
+#     Step2: Pass the step1 to two (1,1) convolutional layer to replace the fully connected layer
+#             classification layer: num_anchors (9 in here) channels for 0, 1 sigmoid activation output
+#             regression layer: num_anchors*4 (36 in here) channels for computing the regression of bboxes with linear activation
+# Args:
+#     base_layers: vgg in here
+#     num_anchors: 9 in here
+
+# Returns:
+#     [x_class, x_regr, base_layers]
+#     x_class: classification for whether it's an object
+#     x_regr: bboxes regression
+#     base_layers: vgg in here
+# """
+def rpn_layer(base_layers, num_anchors):
+    x = Conv2D(512, (3, 3), padding='same', activation='relu', kernel_initializer='normal', name='rpn_conv1')(base_layers)
+
+    x_class = Conv2D(num_anchors, (1, 1), activation='sigmoid', kernel_initializer='uniform', name='rpn_out_class')(x)
+    x_regr = Conv2D(num_anchors * 4, (1, 1), activation='linear', kernel_initializer='zero', name='rpn_out_regress')(x)
+
+    return [x_class, x_regr, base_layers]
